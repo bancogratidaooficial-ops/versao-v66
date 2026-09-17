2347e384c0d3d05106a1f3bc7795eb511320be41
bancogratidaooficial-ops <bancogratidao.oficial@gmail.com>
2026-09-17 03:10:22 -0300

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "plugtv-v66-final-v2";
+const FILES = [
+  "./",
+  "./index.html",
+  "./icon.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => { if (k !== CACHE_NAME) return caches.delete(k); }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
